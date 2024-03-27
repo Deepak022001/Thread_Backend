17a42735ffd9e8153571a71ca61cbbe79fb4a761
deepak022001 <studydipu@gmail.com>
2024-03-27 11:29:45 +0530

    Implemented a folder structure with some error in muatations

---
+++ thread-app-backend/src/graphql/user/index.ts
@@ -0,0 +1,5 @@
+import { mutations } from './mutation';
+import { queries } from './queries';
+import { resolvers } from './resolvers';
+import { typeDefs } from './typedef';
+export const User = { typeDefs, resolvers, queries, mutations };
